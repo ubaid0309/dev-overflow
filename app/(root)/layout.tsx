dbacb7f6fca11442aeafc47813352a7acabb4beb
ubaid0309 <ubaidskms@gmail.com>
2023-12-23 18:52:33 +0530

    add : left sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/shared/Navbar/Navbar";
+import LeftSideBar from "@/components/shared/Sidebar/LeftSideBar";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
@@ -6,7 +7,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <main className="background-light850_dark100 relative">
       <Navbar />
       <div className="flex">
-        Left SideBar
+        <LeftSideBar />
         <section className="flex flex-1 flex-col max-h-screen px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="max-w-5xl w-full mx-auto">{children}</div>
         </section>
---
+++ components/shared/Sidebar/LeftSideBar.tsx
@@ -0,0 +1,94 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { sidebarLinks } from "@/constants";
+import { SignIn, SignedIn } from "@clerk/clerk-react";
+import { SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const LeftSideBar = () => {
+  const pathName = usePathname();
+  return (
+    <section className="left-side-bar max-sm:hidden background-light900_dark200 pt-36 p-6 flex flex-col gap-[14px]">
+      {sidebarLinks.map((link) => {
+        const isActive =
+          (pathName.includes(link?.route) && link?.route.length > 1) ||
+          pathName === link?.route;
+
+        return (
+          <Link
+            href={link?.route}
+            key={link?.route}
+            className={`${
+              isActive
+                ? "primary-gradient text-light-900"
+                : "text-dark300_light900"
+            } flex-start gap-4 p-4 rounded-lg bg-none`}
+          >
+            <Image
+              src={link?.imgURL}
+              width={18}
+              height={18}
+              alt={link?.label}
+              className={isActive ? "" : "invert-colors"}
+            />
+
+            <p
+              className={`${
+                isActive ? "base-bold" : "base-medium"
+              } max-lg:hidden`}
+            >
+              {link?.label}
+            </p>
+          </Link>
+        );
+      })}
+
+      <SignedOut>
+        <Link href="/sign-in">
+          <Button className="outline-none rounded-lg w-full min-h-[41px] small-medium btn-secondary px-4 py-3 shadow-none">
+            <Image
+              src="/assets/icons/account.svg"
+              height={24}
+              width={24}
+              alt="logout"
+              className="lg:hidden invert-colors"
+            />
+            <p className="max-lg:hidden primary-text-gradient">Login</p>
+          </Button>
+        </Link>
+
+        <Link href="/sign-up">
+          <Button className="light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+            <Image
+              src="/assets/icons/sign-up.svg"
+              height={24}
+              width={24}
+              alt="logout"
+              className="lg:hidden invert-colors"
+            />
+            <p className="max-lg:hidden">Sign Up</p>
+          </Button>
+        </Link>
+      </SignedOut>
+
+      <SignedIn>
+        <Link href="/">
+          <Button className="light-border-2 invert-colors btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none flex-start gap-4">
+            <Image
+              src="/assets/icons/arrow-left.svg"
+              height={24}
+              width={24}
+              alt="logout"
+            />
+            <p className="max-lg:hidden">Logout</p>
+          </Button>
+        </Link>
+      </SignedIn>
+    </section>
+  );
+};
+
+export default LeftSideBar;
